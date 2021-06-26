e23ef632faa503de65cccee9367f14bab6f05ec6
Axetroy <axetroy.dev@gmail.com>
2021-06-27 04:35:02 +0800

    update

--- process/find_by_port_darwin.c.v
+++ process/find_by_port_darwin.c.v
@@ -3,14 +3,14 @@ module process
 import os
 import util
 
-pub fn find_process_by_port(port int) ?&Process {
+fn find_process_by_port_from_protocol(port int, protocol string) ?&Process {
 	mut process_list := []&Process{}
 
 	bin_name := 'netstat'
 
 	bin_path := os.find_abs_path_of_executable(bin_name) or { return err }
 	mut ps := os.new_process(bin_path)
-	ps.set_args(['-tunlp'])
+	ps.set_args(['-anv', '-p', protocol])
 	ps.set_redirect_stdio()
 	ps.wait()
 
@@ -21,14 +21,14 @@ pub fn find_process_by_port(port int) ?&Process {
 	table_header_line := 2
 	proto_index := 0
 	addr_index := 3
-	pid_index := 6
+	pid_index := 8
 
 	columns := util.parse_table(output, table_header_line)
 
 	for column in columns {
 		proto := column[proto_index].str()
 		addr := column[addr_index].str()
-		pid := column[pid_index].str().split('/').first()
+		pid := column[pid_index].str()
 
 		if addr.ends_with('.$port') {
 			mut is_exist := false
@@ -58,4 +58,16 @@ pub fn find_process_by_port(port int) ?&Process {
 	}
 
 	return process_list[0]
+}
+
+pub fn find_process_by_port(port int) ?&Process {
+	mut ps := find_process_by_port_from_protocol(port, 'tcp') or {
+		if err.str().starts_with(err_not_found.str()) {
+			return find_process_by_port_from_protocol(port, 'udp')
+		}
+
+		return err
+	}
+
+	return ps
 }
--- process/find_by_port_default.c.v
+++ process/find_by_port_default.c.v
@@ -3,14 +3,14 @@ module process
 import os
 import util
 
-fn find_process_by_port_from_protocol(port int, protocol string) ?&Process {
+pub fn find_process_by_port(port int) ?&Process {
 	mut process_list := []&Process{}
 
 	bin_name := 'netstat'
 
 	bin_path := os.find_abs_path_of_executable(bin_name) or { return err }
 	mut ps := os.new_process(bin_path)
-	ps.set_args(['-anv', '-p', protocol])
+	ps.set_args(['-tunlp'])
 	ps.set_redirect_stdio()
 	ps.wait()
 
@@ -21,14 +21,14 @@ fn find_process_by_port_from_protocol(port int, protocol string) ?&Process {
 	table_header_line := 2
 	proto_index := 0
 	addr_index := 3
-	pid_index := 8
+	pid_index := 6
 
 	columns := util.parse_table(output, table_header_line)
 
 	for column in columns {
 		proto := column[proto_index].str()
 		addr := column[addr_index].str()
-		pid := column[pid_index].str()
+		pid := column[pid_index].str().split('/').first()
 
 		if addr.ends_with('.$port') {
 			mut is_exist := false
@@ -58,16 +58,4 @@ fn find_process_by_port_from_protocol(port int, protocol string) ?&Process {
 	}
 
 	return process_list[0]
-}
-
-pub fn find_process_by_port(port int) ?&Process {
-	mut ps := find_process_by_port_from_protocol(port, 'tcp') or {
-		if err.str().starts_with(err_not_found.str()) {
-			return find_process_by_port_from_protocol(port, 'udp')
-		}
-
-		return err
-	}
-
-	return ps
 }
